926139651a677fe02d9cbb451132a35ff6e47929
hostcdd <315508084@qq.com>
2017-06-12 16:05:26 +0800

    app.js

--- app.js
+++ app.js
@@ -3,5 +3,17 @@
  */
 const express= require('express');
 const app = express();
+// 定义模板引擎，使用swig.renderFile方法解析后缀为html的文件
+const swig=require('swig');
+app.engine('html',swig.renderFile);
 
+// 设置模板存放目录
+app.set('views','./views');
+// 注册模板引擎
+app.set('view engine','html');
+
+swig.setDefaults({cache:false});
+app.get('/user',function (req,res) {
+    res.send('<h1>myBlog</h1>')
+});
 app.listen(9000);
